a2a6da37da29257ccf0b06a55bc07e780e2c194b
Your Name <mwaurarita2019@gmail.com>
2019-10-21 17:10:44 +0300

    Add a view rout path that returour index page

--- main/views.py
+++ main/views.py
@@ -5,3 +5,9 @@ from ..models import Review,User,Pitch
 from flask_login import login_required,current_user
 from .. import db,photos
 import markdown2  
+
+@main.route('/')
+def index():
+    '''
+    view root page function that returns index page
+    '''
