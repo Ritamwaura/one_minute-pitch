9329179549873c633ed2f4b20095ec6c63396faf
Your Name <mwaurarita2019@gmail.com>
2019-10-21 19:04:13 +0300

    Add a decorator that help us update picture

--- main/views.py
+++ main/views.py
@@ -104,7 +104,12 @@ def update_profile(uname):
 def update_pic(uname):
     user = User.query.filter_by(username = uname).first()
     if 'photo' in request.files:
-
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        print(path)
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
 
     
     
