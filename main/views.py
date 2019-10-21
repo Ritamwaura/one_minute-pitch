6b97ecd354941735c3779b180b53a06b0c1e17e1
Your Name <mwaurarita2019@gmail.com>
2019-10-21 17:05:08 +0300

    Add import markdown

--- main/views.py
+++ main/views.py
@@ -1,2 +1,7 @@
 rom flask import render_template,request,redirect,url_for,abort
 from . import main
+from .forms import ReviewForm,UpdateProfile,PitchForm
+from ..models import Review,User,Pitch
+from flask_login import login_required,current_user
+from .. import db,photos
+import markdown2  
