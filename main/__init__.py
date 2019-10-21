60079d1102d988970436dc1d111e61e72065752a
Your Name <mwaurarita2019@gmail.com>
2019-10-21 16:31:11 +0300

    Add innitializing bootstrap aplication

--- main/__init__.py
+++ main/__init__.py
@@ -10,8 +10,14 @@ def create_app(config_name):
     # configure UploadSet
     configure_uploads(app,photos)
 
-# Creating the app configurations
+    # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
-# Initializing flask extensions
+    # Initializing flask extensions
     bootstrap.init_app(app)
+    
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
