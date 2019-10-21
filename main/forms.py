5d48e67f5c7f3dcb7e69df0514d584d576afc881
Your Name <mwaurarita2019@gmail.com>
2019-10-21 16:44:14 +0300

    Add cteat a class that  review a form

--- main/forms.py
+++ main/forms.py
@@ -1,3 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import Required 
+
+class ReviewForm(FlaskForm):
+    title = StringField('Comment')
+    comment = TextAreaField('Enter your comment')
+    submit = SubmitField('submit')
