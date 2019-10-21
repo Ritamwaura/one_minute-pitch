fa2eb646913b38a22c650337fbeef8885464a2f5
Your Name <mwaurarita2019@gmail.com>
2019-10-21 16:21:22 +0300

    Add configuring uploads

--- main/__init__.py
+++ main/__init__.py
@@ -7,3 +7,5 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
     app = Flask(__name__)
+    # configure UploadSet
+    configure_uploads(app,photos)
