c1d55ea164cde985976d61885aa4a9886c09e442
Your Name <mwaurarita2019@gmail.com>
2019-10-21 16:20:36 +0300

    Add configuring uploads

--- main/__init__.py
+++ main/__init__.py
@@ -2,3 +2,8 @@ from flask import Blueprint
 main = Blueprint('main',__name__)
 from . import views,error
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+
+
+photos = UploadSet('photos',IMAGES)
+def create_app(config_name):
+    app = Flask(__name__)
