10b1a6343634f217465abebb82ec4b18084d8eeb
Your Name <mwaurarita2019@gmail.com>
2019-10-21 15:07:43 +0300

    Add a class for regestration form

--- auth/forms.py
+++ auth/forms.py
@@ -4,8 +4,17 @@ from wtforms.validators import Required,Email,Length,EqualTo
 from ..models import User
 from wtforms import ValidationError
 
-class loginForm(FlaskForm)
+class loginForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
     password = PasswordField('Password',validators =[Required()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
+    
+    
+class RegistrationForm(FlaskForm):  
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(),
+    EqualTo('password2',message = 'Passwords must match')])
+    password2 = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
