e8c14321846aa8e065b2ad70ffb03d722be10c6c
Your Name <mwaurarita2019@gmail.com>
2019-10-21 15:05:00 +0300

    Add a class  for login forms

--- auth/forms.py
+++ auth/forms.py
@@ -5,4 +5,7 @@ from ..models import User
 from wtforms import ValidationError
 
 class loginForm(FlaskForm)
-   
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
