5a2575427126c51c0106fa273011795f59d935f7
Your Name <mwaurarita2019@gmail.com>
2019-10-22 12:48:48 +0300

    Add  creating app instance

--- manage.py
+++ manage.py
@@ -2,3 +2,14 @@ from app import create_app, db
 from flask_script import Manager, Shell, Server
 from app.models import User
 from flask_migrate import Migrate, MigrateCommand
+
+# Creating app instance
+# app = create_app('development')
+# app = create_app('test')
+app =creat_app('production')
+
+
+manager = Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('server', Server)
+manager.add_command('db',MigrateCommand)
