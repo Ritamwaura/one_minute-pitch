4c9d60f34b437f237f123424b940f9d21d58e026
Your Name <mwaurarita2019@gmail.com>
2019-10-22 10:45:45 +0300

    Add add a class for user models

--- tests/test_user.py
+++ tests/test_user.py
@@ -2,3 +2,12 @@ import unittest
 from app .models import User
 
 class UserModelsTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(password='password')
+        
+    def test_no_access_password(self):
+        with self .assertRaises(AttributeError): 
+           self.new_new_user.password
+            
+    def  test_password_verification(self):
+         self.assertTrue(self.new_user.verify_password('password'))           
