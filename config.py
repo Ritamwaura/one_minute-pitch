296c7aa2164593a81bae4d53d83987db5561abfa
Your Name <mwaurarita2019@gmail.com>
2019-10-22 10:54:00 +0300

    Add add a class config to get our secreate keys

--- config.py
+++ config.py
@@ -8,3 +8,7 @@ class config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
