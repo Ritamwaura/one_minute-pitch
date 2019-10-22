d2fde6d88fc11ef54bbdafaa1e9500b4cb448fb6
Your Name <mwaurarita2019@gmail.com>
2019-10-22 10:06:40 +0300

    Add add a class for pop the text

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -7,3 +7,10 @@ class BasicsTestCase(unittest.TestCase):
         self.app = create_app('development')
         self.app_context = self.app.app_context()
         self.app_context.push()
+
+def tearDown(self):
+    
+        self.app_context.pop()
+
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
