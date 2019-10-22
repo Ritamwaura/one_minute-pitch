42fe2158dcef8a0c6aa52708210664bb87ee8b73
Your Name <mwaurarita2019@gmail.com>
2019-10-22 09:35:12 +0300

    Add interprete config file in python

--- migrations/env.py
+++ migrations/env.py
@@ -16,3 +16,41 @@ config=context.config
 
 fileConfig(config.config_file_name)
 logger = logging.getLogger('alembic.env')
+
+
+
+
+
+from flask import current_app
+config.set_main_option(
+    'sqlalchemy.url', current_app.config.get(
+        'SQLALCHEMY_DATABASE_URI').replace('%', '%%'))
+target_metadata = current_app.extensions['migrate'].db.metadata
+
+
+
+
+
+
+
+ef run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
