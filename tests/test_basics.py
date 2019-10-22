0f8032b58fb28e6eb0a7cdab1cb09cd9ca108369
Your Name <mwaurarita2019@gmail.com>
2019-10-22 09:58:18 +0300

    Add add a class setup

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,3 +1,9 @@
 import unittest
 from flask import current_app
 from app import create_app
+
+class BasicsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('development')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
