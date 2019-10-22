fe2930cc899c8b63d37a182c1d45bd17c253d32f
Your Name <mwaurarita2019@gmail.com>
2019-10-22 10:52:26 +0300

    Add add a class config to get our secreate keys

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://Rita:123@localhost/pitch'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
